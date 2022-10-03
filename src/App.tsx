49b466dba142715d506a601f37a4753c38a76117
Pedro Barbieri <pedro.romaguera@hotmail.com>
2022-10-03 13:41:31 -0300

    corrigindo português

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
 import { AuthProvider } from './contexts/AuthContext';
 import { Routes } from './routes/Index';
+import SplashScreen from './screens/SplashScreen';
 
 export default function App() {
   return (
